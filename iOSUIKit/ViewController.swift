e207ee124fbd353e24a25d5414fdf25337d39410
fluke <fluke@odds.team>
2025-01-17 11:52:03 +0700

    Initial Commit

---
+++ iOSUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSUIKit
+//
+//  Created by flukeInwza on 17/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
